a16f76e27ebf286733b3753a5d4471436a1f53b4
Edward Diener <eld@tropicsoft.com>
2015-10-11 21:37:32 -0400

    Changed header directory from cxx_dual to cxxd

--- test/test_all.cpp
+++ test/test_all.cpp
@@ -4,7 +4,7 @@
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt).
 
-#include <boost/cxx_dual/cxx_dual.hpp>
+#include <boost/cxxd/cxx_dual.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
 int main()
