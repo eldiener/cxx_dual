cb3853464f86b9afdbd76aafaa1a1810989e650f
Edward Diener <eld@tropicsoft.com>
2016-03-26 23:38:44 -0400

    Updated terminology for CXX modules.

--- include/boost/cxxd/library_name.hpp
+++ include/boost/cxxd/library_name.hpp
@@ -1,4 +1,4 @@
-//  (C) Copyright Edward Diener 2015. 
+//  (C) Copyright Edward Diener 2015-2016.
 //  Use, modification and distribution are subject to the 
 //  Boost Software License, Version 1.0. (See accompanying file 
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -10,7 +10,7 @@
 
 #if BOOST_PP_VARIADICS
 
-#define CXXD_LIBRARY_NAME(libname,...)
+#define CXXD_LIBRARY_NAME(...)
 
 #endif
 
