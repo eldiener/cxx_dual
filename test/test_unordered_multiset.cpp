ded742df1df19912865d7ba0725b39f123cf964a
Edward Diener <eld@tropicsoft.com>
2016-04-25 12:10:42 -0400

    Updated doc and initial addition of tests for mods.

--- test/test_unordered_multiset.cpp
+++ test/test_unordered_multiset.cpp
@@ -1,10 +1,10 @@
 
-//  (C) Copyright Edward Diener 2015
+//  (C) Copyright Edward Diener 2016
 //  Use, modification and distribution are subject to the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt).
 
-#include <boost/cxx_dual/cxx_dual.hpp>
+#include <boost/cxx_dual/unordered_multiset.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
 int main()
