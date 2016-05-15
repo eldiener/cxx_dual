75bd6c8b83890251fda0cbd6944b83f8389976db
Edward Diener <eld@tropicsoft.com>
2016-05-15 11:32:06 -0400

    Updated thread tests. Added test header files previously forgotten. Updated jamfile to run all tests.

--- test/test_mutex.cpp
+++ test/test_mutex.cpp
@@ -4,10 +4,56 @@
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt).
 
+#include <iostream>
+#include <map>
+#include <string>
+#include <boost/foreach.hpp>
+#include <boost/cxx_dual/chrono.hpp>
 #include <boost/cxx_dual/mutex.hpp>
+#include <boost/cxx_dual/thread.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
+#include CXXD_CHRONO_HEADER
+#include CXXD_MUTEX_HEADER
+#include CXXD_THREAD_HEADER
+
+std::map<std::string, std::string> g_pages;
+CXXD_MUTEX_NS::mutex g_pages_mutex;
+ 
+void save_page(const std::string &url)
+{
+    // simulate a long page fetch
+    CXXD_THREAD_NS::this_thread::sleep_for(CXXD_CHRONO_NS::seconds(2));
+    std::string result = "fake content";
+ 
+    CXXD_THREAD_NS::lock_guard<CXXD_MUTEX_NS::mutex> guard(g_pages_mutex);
+    g_pages[url] = result;
+}
+
 int main()
-  {
-  return boost::report_errors();
-  }
+    {
+  
+    CXXD_THREAD_NS::thread t1(save_page, "http://foo");
+    CXXD_THREAD_NS::thread t2(save_page, "http://bar");
+    t1.join();
+    t2.join();
+ 
+#if !defined(BOOST_NO_CXX11_AUTO_DECLARATIONS) && !defined(BOOST_NO_CXX11_RANGE_BASED_FOR)
+ 
+    // safe to access g_pages without lock now, as the threads are joined
+    for (const auto &pair : g_pages) 
+        {
+        std::cout << pair.first << " => " << pair.second << '\n';
+        }
+        
+#else
+
+    typedef std::map<std::string, std::string>::value_type vt;
+
+    BOOST_FOREACH(const vt& pair,g_pages)
+        std::cout << pair.first << " => " << pair.second << '\n';
+      
+#endif
+  
+    return boost::report_errors();
+    }
--- test/test_thread.cpp
+++ test/test_thread.cpp
@@ -4,10 +4,46 @@
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt).
 
+#include <iostream>
+#include <boost/cxx_dual/chrono.hpp>
+#include <boost/cxx_dual/ref.hpp>
 #include <boost/cxx_dual/thread.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
+#include CXXD_CHRONO_HEADER
+#include CXXD_REF_HEADER
+#include CXXD_THREAD_HEADER
+
+void f1(int n)
+{
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Thread 1 executing\n";
+        ++n;
+        CXXD_THREAD_NS::this_thread::sleep_for(CXXD_CHRONO_NS::milliseconds(10));
+    }
+}
+ 
+void f2(int& n)
+{
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Thread 2 executing\n";
+        ++n;
+        CXXD_THREAD_NS::this_thread::sleep_for(CXXD_CHRONO_NS::milliseconds(10));
+    }
+}
+
 int main()
-  {
-  return boost::report_errors();
-  }
+    {
+  
+    int n = 0;
+    CXXD_THREAD_NS::thread t1; // t1 is not a thread
+    CXXD_THREAD_NS::thread t2(f1, n + 1); // pass by value
+    CXXD_THREAD_NS::thread t3(f2, CXXD_REF_NS::ref(n)); // pass by reference
+//    std::thread t4(std::move(t3)); // t4 is now running f2(). t3 is no longer a thread
+    t2.join();
+    t3.join();
+//    t4.join();
+    std::cout << "Final value of n is " << n << '\n';
+  
+    return boost::report_errors();
+    }
