03eed05350ce65d50d4d43a3e4c6ee572096898c
Edward Diener <eld@tropicsoft.com>
2016-05-31 18:02:43 -0400

    Create branch for documentation

--- include/boost/cxx_dual/detail/shared_ptr.hpp
+++
@@ -1,9 +0,0 @@
-//  (C) Copyright Edward Diener 2015-2016. 
-//  Use, modification and distribution are subject to the 
-//  Boost Software License, Version 1.0. (See accompanying file 
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#include <boost/shared_ptr.hpp>
-#include <boost/weak_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/make_shared.hpp>
